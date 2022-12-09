ed5cc1dd393fb575ee69480b5c792f32a1a8ee9a
Bri Douglas <116006373+bdougla4@users.noreply.github.com>
2022-12-09 18:52:02 -0500

    Update app.py
    adding HTTPS functionality and removing HTTP. updating password requirements

--- app.py
+++ app.py
@@ -124,9 +124,10 @@ def register():
                 return render_template('login.html', msg=msg)
             elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
                 msg = 'Invalid email address !'
-            elif not re.match(r'[A-Za-z0-9@#$%^&+=]{8,}', escape(request.form['password'])):
+            elif len(request.form['password']) < 8 or\
+                re.search((r"(\#)+|(\*)+|\!+|\&+\@+\$+"), escape(request.form['password'])) == None:
                 msg = 'Passwords must be a minimum of 8 characters, contain lower and upper case letters, numbers ' \
-                      'and at least one special character (!, @, # $, ^, % *).'
+                      'and at least one special character (acceptable characters: #, *, ! &, @, $).'
             elif not username or not password or not email:
                 msg = 'Please complete all required fields.'
             else:
@@ -148,6 +149,158 @@ def register():
         logging.info("Registration failed for user. Not all fields completed.")
     return render_template('register.html', msg=msg)
 
+@app.route('/menu/', methods=['GET', 'POST'])
+def menu():
+    activeGame = False
+    logging.debug("checking if user has active game")
+    dbCur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    if Games.get_all_active_games_for_single_user_id(dbCur, userid) != None:
+        activeGame = True 
+        logging.info("user has active game")
+
+    logging.info("active game = %s", activeGame)
+    return render_template('menu.html', activeGame=activeGame, gameForfeited=False)
+
+@app.route('/game/', methods=['GET', 'POST'])
+def game():
+    displayUndefinedError = None
+    displayForfeitError = None
+    playerStats = None
+    try:
+        dbCur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+        # used in the case that a user enters a non defined word and old stats need to stay on board
+        if ('gameStatus' in request.args):
+            gameStatus = request.args.get('gameStatus')
+            if gameStatus == 'continue':
+                logging.debug("user wants to continue previous game. checking if user has active game")
+                # TO-DO: get user id
+                currentGame = Games.get_all_active_games_for_single_user_id(dbCur, userid)
+                if currentGame != None:
+                    logging.info("user has active game")
+                    currentUsersTurn = currentGame['current_users_turn']
+                    playerStats = GamePlay.generate_continue_game_stats(dbCur, currentGame)
+                else:
+                    raise UserForfeitedException("Other user forfeited during game play")
+        if ('submit-user-input' in request.form and 'user-word' in request.form and 
+        'user-position' in request.form and 'col' in request.form and 'row' in request.form):
+            logging.debug('user submitted position, word, row, and column')
+            position = str(escape(request.form["user-position"]))
+            word = str(escape(request.form["user-word"]))
+            col = str(escape(request.form["col"]))
+            row = str(escape(request.form["row"]))
+            # TO-DO: make sure it is user's turn when inserting move
+            # TO-DO: get user id
+            playerStats = GamePlay.handle_users_input(GamePlay, dbCur, currentGame['game_id'], currentUsersTurn, word, position, col, row)
+            db.connection.commit()
+    except UserForfeitedException as err:
+        logging.warning("Other user forfeited during game play. Displaying error to UI")
+        displayForfeitError = True
+    except UndefinedWordException as err:
+        logging.warning("User's input was invalid. Displaying error to UI")
+        displayUndefinedError = True
+
+            
+    return render_template('game.html', gameStatus='continue', playerStats=playerStats, 
+        displayUndefinedError=displayUndefinedError, displayForfeitError=displayForfeitError)
+
+@app.route('/new-game/', methods=['GET', 'POST'])
+def newGame():
+    # TO-DO: get userid from actual user
+    dbCur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    logging.debug("user: %s is requesting to join new game", userid)
+
+    logging.debug("Making sure user does not have an active game already")
+    # TO-DO get the correct userid to check for active games
+    activeGames = Games.get_all_active_games_for_single_user_id(dbCur, userid)
+
+    if activeGames != None:
+        logging.warn('User: %s is requesting a new game but already has an active one running', userid)
+    else:
+        logging.debug('User has no active games currently. Creating a new game')
+        # TO-DO: how to get a second user to join this game?
+        # TO-DO get the correct userids
+        gameId = Games.add_game(dbCur, userid, userid2)
+        db.connection.commit()
+
+        newGame = Games.get_game_by_id(dbCur, gameId)
+        playerStats = GamePlay.generate_new_game_stats(dbCur, newGame)
+        return render_template('game.html', gameStatus='newGame', playerStats=playerStats)
+    
+
+@app.route('/end-game/', methods=['GET', 'POST'])
+def forfeitGame():
+    # TO-DO: get userid from actual user
+    dbCur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    logging.info("user: %s forfeited game", userid)
+
+    # TO-DO get the correct userid to mark as winner
+    Games.game_finished(Games, dbCur, userid, userid2, userid2)
+
+    # TO-DO get the correct userid to mark as winner
+    Users.set_user_lost(Users, dbCur, userid)
+
+    # TO-DO get the correct userid to mark as winner
+    Users.set_user_won(Users, dbCur, userid2)
+
+    db.connection.commit()
+    return render_template('menu.html', activeGame=False, gameForfeited=True)
+
+@app.route('/scoreboard/', methods=['GET', 'POST'])
+def scoreboard():
+    userScore = request.args.get('user-score')
+    if request.method == 'GET' and (('username' in request.args) or 
+    ('user-score' in request.args) or ('self-score' in request.args)):
+        if((userScore == None) or (userScore == '')):
+            username = 'user1'
+            logging.debug('user: %s wants to see their own personal scores', username)
+            return generateScoreboard(username)
+
+        username = request.args.get('username')
+        if (username != ''):
+            username = str(escape(username))
+            logging.info('user is requesting score for user: %s', username)
+            # if ((re.match(r'[^@]+@[^@]+\.[^@]+', username)) or (re.match(r'[A-Za-z]{1,50}', username))):
+
+            if re.match(r'[A-Za-z0-9]{1,50}', username):
+                logging.info('%s is good username format', username)
+                # userScore = request.args.get('user-score')     
+                return generateScoreboard(username)
+            else:
+                logging.warn('%s is bad username format', username)
+  
+    return render_template('scoreboard.html')
+
+def generateScoreboard(username):
+    noScores = False
+    userFinalScores = None
+    dbCur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+
+    logging.debug('getting score for user: %s', username)
+    # TO-DO: get username for current user
+    scoresRetrieved = Users.get_user_score_board(Users, dbCur, username)
+    if scoresRetrieved != []:
+        # TO-DO: get username for current user
+        userFinalScores = Users.get_user_by_user_name(Users, dbCur, username)
+    else:
+        noScores = True
+
+    return render_template('scoreboard.html', scoresRetrieved=scoresRetrieved, username=username, userFinalScores=userFinalScores, noScores=noScores) 
+
+
+@app.route('/moves/', methods=['GET', 'POST'])
+def getMoves():
+    gameId = request.args.get('gameId')
+    logging.debug('getting moves for gameId: %s', gameId)
+
+    dbCur = db.connection.cursor(MySQLdb.cursors.DictCursor)
+    gameMoves = Moves.get_all_moves_for_game(dbCur, gameId)
+
+    print(gameMoves)
+    return render_template('moves.html', gameMoves=gameMoves)
 
 if __name__ == "__main__":
-    app.run()
+    # This will force all communications to be exchanged via HTTPS rather than HTTP. 
+    app.run(ssl_context="adhoc")
+    
+    # Upon generation of ssl certs, we will comment out the app.run(ssl_context="adhoc") above and use this line to use our self-signed certificates.
+    # app.run(ssl_context='isa681Cert.pem', 'isa681Key.pem')
